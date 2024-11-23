4ddeaf62a5dcd1b8c081ef931bf8ff5dd370634f
AnujPratap-96 <offcialthakur94@gmail.com>
2024-11-23 23:06:38 +0530

    "api name correction"

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,22 +1,22 @@
 export const exerciseOptions = {
-    method: 'GET',
-    headers: {
-        'x-rapidapi-key': import.meta.env.VITE_RAPID_API_KEY_EXERCISE,
-        'x-rapidapi-host': 'exercisedb.p.rapidapi.com',
-    },
-  };
-  
-  export const youtubeOptions = {
-    method: 'GET',
-    headers: {
-          'x-rapidapi-key': import.meta.env.VITE_RAPID_API_KEY_YOUTUBE,
-    'x-rapidapi-host': 'youtube-search-and-download.p.rapidapi.com'
-    },
-  };
-  
-  export const fetchData = async (url, options) => {
-    const res = await fetch(url, options);
-    const data = await res.json();
-  
-    return data;
-  };
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": import.meta.env.VITE_RAPID_API,
+    "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+  },
+};
+
+export const youtubeOptions = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": import.meta.env.VITE_RAPID_API,
+    "x-rapidapi-host": "youtube-search-and-download.p.rapidapi.com",
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
